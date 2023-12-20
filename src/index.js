ae463c0270a210b400e7a3392a9d0b3ab9dfe1eb
roLLlyTHuK <ircvipua@gmail.com>
2023-12-20 15:06:10 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-import React, {createContext} from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import firebase from "firebase";
-import 'firebase/firestore'
-import 'firebase/auth'
+import "firebase/firestore";
+import "firebase/auth";
 
 firebase.initializeApp({
-        apiKey: "AIzaSyBr1JWix2dc-VoBCB7fHVSGS9KUL0X0Wis",
-        authDomain: "chat-react-fd366.firebaseapp.com",
-        projectId: "chat-react-fd366",
-        storageBucket: "chat-react-fd366.appspot.com",
-        messagingSenderId: "1097930185910",
-        appId: "1:1097930185910:web:206a1158ff7075b6381a17",
-        measurementId: "G-Q9W58YQ4XL"
-    }
-);
+  apiKey: "AIzaSyCIUfGDBX1DIVBNZaxvymjDNQ4-0drze7I",
+  authDomain: "chat-react-2062b.firebaseapp.com",
+  projectId: "chat-react-2062b",
+  storageBucket: "chat-react-2062b.appspot.com",
+  messagingSenderId: "404111829251",
+  appId: "1:404111829251:web:07f61448e71d8e9867a136",
+});
 
-export const Context = createContext(null)
+export const Context = createContext(null);
 
-const auth = firebase.auth()
-const firestore = firebase.firestore()
-
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 ReactDOM.render(
-    <Context.Provider value={{
-        firebase,
-        auth,
-        firestore
-    }}>
-        <App />
-    </Context.Provider>,
-  document.getElementById('root')
+  <Context.Provider
+    value={{
+      firebase,
+      auth,
+      firestore,
+    }}
+  >
+    <App />
+  </Context.Provider>,
+  document.getElementById("root")
 );
-
